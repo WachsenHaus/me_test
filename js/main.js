8b005ecadb19e67ef738f3d30b67b59bf93d8933
WachsenHaus <59411545+WachsenHaus@users.noreply.github.com>
2020-06-07 22:50:32 +0900

    js home contact btn

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//navbar 선택시 해당 element로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", () => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+//컨택트버튼클릭시 해당 요소로 이동
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
